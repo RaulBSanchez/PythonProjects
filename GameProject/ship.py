f11a6fec5f95e6849e6d5865d5abb706e2a4e5a1
Raul Bazan Sanchez <47358801+RaulBSanchez@users.noreply.github.com>
2020-05-08 09:30:48 -0400

    add left movement

--- GameProject/ship.py
+++ GameProject/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         #Initilize the ship and set its starting postion
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -15,12 +16,25 @@ class Ship:
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # store a decimal value for the ships horizonal pattern
+        self.x = float(self.rect.x)
+
+
+        #movement flags
         self.moving_right = False
+        self.moving_left = False
+
+        self.ship_speed = 1.5
 
     def update(self):
         # Update the ships postioin based on the movement flag
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x
+        self.rect.x = self.x
     def blitme(self):
         #draw ship at the current location
         self.screen.blit(self.image, self.rect)
