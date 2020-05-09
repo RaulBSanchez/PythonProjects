916b48e921ec913e0059400536e5f83faf3a921b
Raul Bazan Sanchez <47358801+RaulBSanchez@users.noreply.github.com>
2020-05-09 16:18:24 -0400

    Add fire power

--- GameProject/settings.py
+++ GameProject/settings.py
@@ -6,4 +6,10 @@ class Settings:
         self.bg_color = [230, 230, 230]
         self.screen_width = 1000
         self.screen_length = 400
+        self.ship_speed = 1.5
 
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = [60, 60, 60]
